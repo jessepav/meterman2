e8c30b437f042c249b09d80df647b27ebb26cc13
Jesse Pavel <jpavel@alum.mit.edu>
2019-02-11 03:32:04 +0000

    Started doc comment for BundleGroup

--- src/com/illcode/meterman2/BundleGroup.java
+++ src/com/illcode/meterman2/BundleGroup.java
@@ -1,5 +1,9 @@
 package com.illcode.meterman2;
 
+/**
+ * A BundleGroup maintains a list of {@code XBundle}S and supports operations that
+ * query each in order, as though the group were one big {@code XBundle}.
+ */
 public final class BundleGroup
 {
 }
