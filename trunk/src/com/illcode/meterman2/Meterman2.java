188e98d804c0ea2448945bfb0637e05ecc4d9f54
jpavel <jpavel@0f3d5359-7e7c-457c-8d96-54a8d0809d2c>
2019-02-14 02:27:27 +0000

    Starting to build up the program a la MM1

--- trunk/src/com/illcode/meterman2/Meterman2.java
+++ trunk/src/com/illcode/meterman2/Meterman2.java
@@ -1,8 +1,72 @@
 package com.illcode.meterman2;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.logging.Level;
+
+import static com.illcode.meterman2.MMLogging.logger;
+
 public final class Meterman2
 {
-    public static void main(String[] args) {
+    public static Path prefsPath, savesPath, assetsPath, gluePath;
+    static Properties prefs;
 
+    public static void main(String[] args) throws IOException {
+        prefsPath = Paths.get("config/meterman2.properties");
+        if (!loadPrefs(prefsPath)) {
+            System.err.println("Error loading prefs from " + prefsPath.toString());
+            System.exit(1);
+        }
+
+        MMLogging.initializeLogging();
+
+        savesPath = Paths.get(Utils.pref("saves-path", "saves"));
+        if (Files.notExists(savesPath))
+            Files.createDirectories(savesPath);
+
+        assetsPath = Paths.get(Utils.pref("assets-path", "assets"));
+        if (Files.notExists(assetsPath)) {
+            logger.severe("Assets path doesn't exist!");
+            return;
+        }
+
+        gluePath = Paths.get(Utils.pref("glue-path", "glue"));
+        if (Files.notExists(gluePath)) {
+            logger.severe("Glue path doesn't exist!");
+            return;
+        }
+
+    }
+
+    private static boolean loadPrefs(Path path) {
+        prefs = new Properties();
+        if (Files.exists(path)) {
+            try (FileReader r = new FileReader(path.toFile())) {
+                prefs.load(r);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void savePrefs(Path path) {
+        try (FileWriter w = new FileWriter(path.toFile())) {
+            prefs.store(w, "Meterman2 preferences");
+        } catch (IOException ex) {
+            logger.log(Level.WARNING, "savePrefs " + path.toString(), ex);
+        }
+    }
+
+    /** Called when the program is shutting down. */
+    public static void shutdown() {
+        logger.info("Meterman shutting down...");
+        savePrefs(prefsPath);
     }
 }
