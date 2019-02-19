363e616aa93f7fa4762e13d6a48b4ab16ff33bc6
jpavel <jpavel@0f3d5359-7e7c-457c-8d96-54a8d0809d2c>
2019-02-19 17:42:42 +0000

    Add a custom TemplateExceptionHandler

--- trunk/src/com/illcode/meterman2/MMTemplate.java
+++ trunk/src/com/illcode/meterman2/MMTemplate.java
@@ -2,10 +2,17 @@ package com.illcode.meterman2;
 
 import freemarker.cache.MruCacheStorage;
 import freemarker.cache.StringTemplateLoader;
+import freemarker.core.Environment;
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapperBuilder;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+
+import static com.illcode.meterman2.MMLogging.logger;
 
 import java.io.IOException;
+import java.io.Writer;
+import java.util.logging.Level;
 
 /**
  * This class handles Meterman's interaction with a scripting engine, in our case FreeMarker.
@@ -19,6 +26,7 @@ public class MMTemplate
         cfg = new Configuration(Configuration.VERSION_2_3_28);
         cfg.setDefaultEncoding("UTF-8");
         cfg.setLogTemplateExceptions(false);
+        cfg.setTemplateExceptionHandler(new MMTemplateExceptionHandler());
         DefaultObjectWrapperBuilder b = new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_28);
         b.setForceLegacyNonListCollections(false);
         b.setExposeFields(true);
@@ -41,6 +49,17 @@ public class MMTemplate
             cfg.removeTemplateFromCache(templateName);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static class MMTemplateExceptionHandler implements TemplateExceptionHandler
+    {
+        public void handleTemplateException(TemplateException te, Environment env, Writer out) throws TemplateException {
+            try {
+                out.write("[ERROR: " + te.getMessage() + "]");
+            } catch (IOException e) {
+                logger.log(Level.WARNING, "Could not write TemplateException: ", te);
+            }
         }
     }
 }
