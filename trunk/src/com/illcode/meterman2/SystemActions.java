51bfe727c71a65fcbbfe7d5ef3f0f18376fa3734
jpavel <jpavel@0f3d5359-7e7c-457c-8d96-54a8d0809d2c>
2019-02-26 20:09:57 +0000

    Wrote an action system (MMActions)

---
+++ trunk/src/com/illcode/meterman2/SystemActions.java
@@ -0,0 +1,15 @@
+package com.illcode.meterman2;
+
+import static com.illcode.meterman2.MMActions.Action;
+
+public class SystemActions
+{
+    public static Action LOOK;
+    public static Action WAIT;
+
+    public static void init() {
+        LOOK = Meterman2.actions.registerAction("Look");
+        WAIT = Meterman2.actions.registerAction("Wait");
+        Meterman2.actions.markSystemActionsDone();
+    }
+}
