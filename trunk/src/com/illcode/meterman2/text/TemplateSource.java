762a45e205acb79e4fb94fe84a4e999d0d84ef6c
jpavel <jpavel@0f3d5359-7e7c-457c-8d96-54a8d0809d2c>
2019-02-26 03:18:46 +0000

    Starting on the bundle and text infrastructure

--- trunk/src/com/illcode/meterman2/text/TemplateSource.java
+++ trunk/src/com/illcode/meterman2/text/TemplateSource.java
@@ -4,7 +4,11 @@ package com.illcode.meterman2.text;
  * A {@code TextSource} whose output is rendered by processing a (FreeMarker) template.
  */
 
-public class TemplateSource
+public class TemplateSource implements TextSource
 {
     private String templateName;
+
+    public String getText() {
+        return null;
+    }
 }
