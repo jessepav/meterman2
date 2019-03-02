f3a7bcf5b023a3bb82eefde60fdf756eaa8d2932
jpavel <jpavel@0f3d5359-7e7c-457c-8d96-54a8d0809d2c>
2019-03-02 21:08:38 +0000

    Scripting seems to work, yeah!

--- trunk/src/com/illcode/meterman2/MMLogging.java
+++ trunk/src/com/illcode/meterman2/MMLogging.java
@@ -5,10 +5,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 public final class MMLogging
 {
@@ -32,4 +29,21 @@ public final class MMLogging
         logger.info("Meterman2 Logger Initialized. Default Charset: " + Charset.defaultCharset());
     }
 
+    /**
+     * In case someone wants to use use {@link #logger} to print to the console without the full rotating
+     * log machinery that we normally set up.
+     * @param name name of the logger
+     */
+    public static void initSimpleLogging(String name) throws IOException {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: [%1$tF %1$tT] %5$s %6$s%n");
+        LogManager.getLogManager().readConfiguration();
+
+        logger = Logger.getLogger(name);
+        logger.setLevel(Level.FINE);
+        logger.setUseParentHandlers(false);
+        Handler handler = new ConsoleHandler();
+        handler.setLevel(Level.FINE);
+        handler.setFormatter(new SimpleFormatter());
+        logger.addHandler(handler);
+    }
 }
--- trunk/src/com/illcode/meterman2/text/ScriptSource.java
+++ trunk/src/com/illcode/meterman2/text/ScriptSource.java
@@ -1,25 +1,60 @@
 package com.illcode.meterman2.text;
 
 import bsh.NameSpace;
+import com.illcode.meterman2.MMScript;
+import com.illcode.meterman2.MMScript.ScriptedMethod;
+import com.illcode.meterman2.Meterman2;
+import com.illcode.meterman2.bundle.XBundle;
+
+import java.util.List;
 
 /**
  * A {@code TextSource} whose output is determined by running a script.
+ * <p/>
+ * A ScriptSource can optionally reference an {@link XBundle}, in which case the text
+ * output will be formatted according to the settings of that XBundle by calling
+ * {@link XBundle#formatText(String)}.
  */
 public class ScriptSource implements TextSource
 {
     private final String id;
     private final String source;
-    private NameSpace ns;
+    private ScriptedMethod method;
+    private XBundle bundle;
 
     public ScriptSource(String id, String source) {
         this.id = id;
         this.source = source;
     }
 
+    public ScriptSource(String id, String source, XBundle bundle) {
+        this(id, source);
+        this.bundle = bundle;
+    }
+
+    /**
+     * Set the XBundle referenced by this ScriptSource.
+     * @param bundle bundle used to format the template output; if null, no additional
+     *               formatting will be performed.
+     */
+    public void setBundle(XBundle bundle) {
+        this.bundle = bundle;
+    }
+
     public String getText() {
-        if (ns == null) {  // this is the first time we're invoked
-            //ns =
+        if (method == null) {  // this is the first time we're invoked
+            StringBuilder sb = new StringBuilder(source.length() + 100);
+            sb.append("void getScriptedText() {\n");
+            sb.append(source);
+            sb.append("\n}");
+            List<ScriptedMethod> methods = Meterman2.script.evalScript(id, sb.toString());
+            if (methods.isEmpty())
+                return "Error in ScriptSource ID: " + id;
+            method = methods.get(0);
         }
-        return null;
+        String output = method.invoke().getRight();
+        if (bundle != null)
+            output = bundle.formatText(output);
+        return output;
     }
 }
