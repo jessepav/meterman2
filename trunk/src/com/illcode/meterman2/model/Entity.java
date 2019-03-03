ede31fec41d5b3d25774ee6c12aafadf0e32db44
jpavel <jpavel@0f3d5359-7e7c-457c-8d96-54a8d0809d2c>
2019-03-03 21:49:05 +0000

    Working on the game infrastructure

--- trunk/src/com/illcode/meterman2/model/Entity.java
+++ trunk/src/com/illcode/meterman2/model/Entity.java
@@ -1,5 +1,13 @@
 package com.illcode.meterman2.model;
 
+/**
+ * The "interface" through which the game system and UI interacts with entities.
+ * <p/>
+ * Entity is a class rather than an interface because it supports method delegation; all other
+ * implementation is handled by an instance of {@link EntityImpl}. It is that interface,
+ * and its base implementation {@code BaseEntityImpl}, that specialized entities will
+ * usually extend.
+ */
 public class Entity
 {
 }
