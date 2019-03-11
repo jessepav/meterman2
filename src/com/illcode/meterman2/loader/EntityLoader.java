d4ce2c737787dd85b44e88313051042b8fc4dd1f
Jesse Pavel <jpavel@alum.mit.edu>
2019-03-11 19:54:23 +0000

    Building up loaders

--- src/com/illcode/meterman2/loader/EntityLoader.java
+++ src/com/illcode/meterman2/loader/EntityLoader.java
@@ -8,11 +8,22 @@ public interface EntityLoader
 {
     /**
      * Called by the game system to construct and return an Entity (or subclass) with
-     * properties defined in an XBundle.
+     * properties defined in an XBundle element.
      * @param bundle XBundle that contains the element
      * @param el the XML element containing the entity defintion
      * @param id ID for the new entity
      * @return a new entity
      */
     Entity loadFromXml(XBundle bundle, Element el, String id);
+
+    /**
+     * Load values for the various entity properties from an XML element.
+     * Subclasses of EntityLoader implementations can chain up to their parent's method to load standard entity
+     * properties before loading subclass-specific properties. The implementation of hot-reloading
+     * will likely end up using this method as well.
+     * @param bundle XBundle that contains the element
+     * @param el the XML element containing the entity defintion
+     * @param e the entity whose properties will be set from values in the XML element.
+     */
+    void loadPropertiesFromXml(XBundle bundle, Element el, Entity e);
 }
