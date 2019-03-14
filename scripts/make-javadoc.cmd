771ce170eabff5fb41ca4b4fbc12df8e3cc68dc6
Jesse Pavel <jpavel@alum.mit.edu>
2019-03-14 16:59:26 +0000

    Javadoc build system

--- scripts/make-javadoc.cmd
+++ scripts/make-javadoc.cmd
@@ -20,8 +20,9 @@ javadoc -classpath lib\*;build\production\meterman2 -%visibility% %linksrc% ^
    -sourcepath src -subpackages com.illcode.meterman2 -d "%output_dir%" ^
    -windowtitle "Meterman2 Javadocs" -doctitle "Meterman2 Javadocs" ^
    -linkoffline https://docs.oracle.com/javase/7/docs/api/ scripts\package-info\java7 ^
-   -linkoffline https://jessepav.github.io/java-api-docs/BeanShell/javadoc/ scripts\package-info\bsh
-   -linkoffline http://static.javadoc.io/org.jdom/jdom2/2.0.6/ scripts\package-info\jdom2
+   -linkoffline https://jessepav.github.io/java-api-docs/bsh-2.1.9-JP-javadoc/ scripts\package-info\bsh ^
+   -linkoffline http://static.javadoc.io/org.jdom/jdom2/2.0.6/ scripts\package-info\jdom2 ^
+   -linkoffline http://static.javadoc.io/org.freemarker/freemarker/2.3.28/ scripts\package-info\freemarker
 
 :END
 ENDLOCAL
