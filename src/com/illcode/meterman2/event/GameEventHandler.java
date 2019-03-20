9b22b18e913211d97138fc2400be895946942a26
Jesse Pavel <jpavel@alum.mit.edu>
2019-03-20 17:53:12 +0000

    Remove special prefixes for room and entity GameEventHandlerS

--- src/com/illcode/meterman2/event/GameEventHandler.java
+++ src/com/illcode/meterman2/event/GameEventHandler.java
@@ -8,22 +8,6 @@ import com.illcode.meterman2.model.Game;
 public interface GameEventHandler
 {
     /**
-     * If an event handler's ID begins with this prefix, it indicates that the handler is implemented by
-     * a Room, so instead of calling {@link Game#getEventHandler(String)} when restoring game state,
-     * the system will use the room with an ID derived by stripping this prefix from the handler ID.
-     * That Room must implement the appropriate GameEventHandler sub-interface.
-     */
-    String ROOM_EVENT_HANDLER_PREFIX = "roomId:";
-
-    /**
-     * If an event handler's ID begins with this prefix, it indicates that the handler is implemented by
-     * an Entity, so instead of calling {@link Game#getEventHandler(String)} when restoring game state,
-     * the system will use the entity with an ID derived by stripping this prefix from the handler ID.
-     * That Entity must implement the appropriate GameEventHandler sub-interface.
-     */
-    String ENTITY_EVENT_HANDLER_PREFIX = "entityId:";
-
-    /**
      * Return the ID of this GameEventHandler. The ID is used to persist the active listeners when a game is
      * saved and subsequently loaded, without needing to serialize the actual handler objects.
      */
