099a9084fdf127e62964b975fbc78665ca951daf
jessepav <jessepav@0f3d5359-7e7c-457c-8d96-54a8d0809d2c>
2019-03-28 02:42:28 +0000

    Refactoring talk infrastructure

--- trunk/src/com/illcode/meterman2/model/Talker.java
+++ trunk/src/com/illcode/meterman2/model/Talker.java
@@ -2,50 +2,19 @@ package com.illcode.meterman2.model;
 
 import com.illcode.meterman2.model.TopicMap.Topic;
 
-import java.util.Collection;
-
 /**
- * A game entity that can be talked to. Classes should delegate these methods
- * to an instance of {@link TalkSupport} in implementing this interface, for desired behavior:
- * <ul>
- *     <li>getTopicMap()</li>
- *     <li>setTopicMap()</li>
- *     <li>talk()</li>
- *     <li>addTopic()</li>
- *     <li>removeTopic()</li>
- *     <li>clearTopics()</li>
- * </ul>
- * and implement these themselves (or delegate them to scripts) to perform custom processing
- * <ul>
- *     <li>getTalkerEntity()</li>
- *     <li>topicChosen()</li>
- *     <li>getOtherTopicLabel()</li>
- *     <li>talkOther()</li>
- * </ul>
+ * A game entity that can be talked to.
  */
 public interface Talker
 {
-    /** Return the entity that is implementing Talker. */
+    /** Return the entity that is implementing, or bound to this, Talker. */
     Entity getTalkerEntity();
 
-    /**
-     * Return the topic map being used by this Talker.
-     */
-    TopicMap getTopicMap();
+    /** Return the talk-support instance being used by this Talker. */
+    TalkSupport getTalkSupport();
 
     /**
-     * Set the topic map being used by this Talker.
-     */
-    void setTopicMap(TopicMap topicMap);
-
-    /**
-     * Entry point to the conversation system, called by the game engine when the user
-     * selects the Talk action on this entity.
-     */
-    void talk();
-
-    /**
-     * Hook method called when the user has selected one of the active topics.
+     * Called when the user has selected one of the active topics.
      * @param t topic chosen
      * @return false to show the topic text and process add- and remove-topics, as usual;
      * true if the Talker has handled this topic itself and wants to skip normal processing.
@@ -64,21 +33,4 @@ public interface Talker
      * @param topic user-input topic
      */
     void talkOther(String topic);
-
-    /**
-     * Add a topic to our list of current topics.
-     * @param id topic ID, as found in the topic map.
-     */
-    void addTopic(String id);
-
-    /**
-     * Remove a topic from our list of current topics.
-     * @param id topic ID, as found in the topic map.
-     */
-    void removeTopic(String id);
-
-    /**
-     * Clear all current topics.
-     */
-    void clearTopics();
 }
