4c4aa9ea1c4ca3a85de2685ff4c0b778d29faf6d
jessepav <jessepav@0f3d5359-7e7c-457c-8d96-54a8d0809d2c>
2019-04-12 23:04:22 +0000

    Fix run.bat

--- trunk/scripts/run.bat
+++ trunk/scripts/run.bat
@@ -2,5 +2,5 @@
 pushd %~dp0..
 java -Xmx256M -cp build\production\meterman2;build\production\riverboat;lib\*;jars\* ^
     -splash:assets/meterman2/splash-screen.png ^
-    com.illcode.meterman2.Meterman
+    com.illcode.meterman2.Meterman2
 popd
