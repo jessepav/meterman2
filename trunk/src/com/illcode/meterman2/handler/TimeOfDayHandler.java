021653b0ca8c0bf8fc31fd11ccf5788b58d659ec
jessepav <jessepav@0f3d5359-7e7c-457c-8d96-54a8d0809d2c>
2019-04-12 19:39:17 +0000

    Improvements to TimeOfDayHandler

--- trunk/src/com/illcode/meterman2/handler/TimeOfDayHandler.java
+++ trunk/src/com/illcode/meterman2/handler/TimeOfDayHandler.java
@@ -15,19 +15,21 @@ public class TimeOfDayHandler implements TurnListener, StatusBarProvider
 
     protected int secondsSinceMidnight;
     protected int secondsPerTurn;
-    protected boolean format24h;
+    protected boolean format24h, showSeconds;
 
     private int statusLabelPos;
+    private int[] timeArray;
 
     /**
-     * Construct a new time-of-day handler, with the current time set to midnight.
+     * Construct a new time-of-day handler, with the current time set to midnight and one minute
+     * elapsing per turn.
      * @param handlerId handler ID
-     * @param secondsPerTurn the number of seconds that elapse each turn.
      */
-    public TimeOfDayHandler(String handlerId, int secondsPerTurn) {
+    public TimeOfDayHandler(String handlerId) {
         this.handlerId = handlerId;
-        this.secondsPerTurn = secondsPerTurn;
+        secondsPerTurn = 60;
         statusLabelPos = UIConstants.RIGHT_LABEL;
+        timeArray = new int[3];
     }
 
     /** Registers this time-of-day handler with the game manager.  */
@@ -41,28 +43,13 @@ public class TimeOfDayHandler implements TurnListener, StatusBarProvider
     }
 
     /** Get the number of seconds that elapse each turn. */
-    public int getSecondsPerTurn() {
+    final public int getSecondsPerTurn() {
         return secondsPerTurn;
     }
 
-    /** Set the number of seconds that elapse each turn. */
-    public void setSecondsPerTurn(int secondsPerTurn) {
-        this.secondsPerTurn = secondsPerTurn;
-    }
-
-    /** Set the number of minutes that elapse each turn. */
-    public void setMinutesPerTurn(int minutesPerTurn) {
-        this.secondsPerTurn = minutesPerTurn * 60;
-    }
-
-    /** Returns the number of seconds elapsed since midnight. */
-    public int getTime() {
-        return secondsSinceMidnight;
-    }
-
-    /** Returns the hour of day (0-23). */
-    public int getHourOfDay() {
-        return secondsSinceMidnight / 3600;
+    /** Set the amount of time that elapses each turn. */
+    final public void setTimePerTurn(int hours, int minutes, int seconds) {
+        this.secondsPerTurn = (hours * 3600 + minutes * 60 + seconds) % SECONDS_PER_DAY;
     }
 
     /**
@@ -71,13 +58,40 @@ public class TimeOfDayHandler implements TurnListener, StatusBarProvider
      * @param minutes minute of hour (0-59)
      * @param seconds second of minutes (0-59)
      */
-    public void setTime(int hours, int minutes, int seconds) {
+    final public void setTime(int hours, int minutes, int seconds) {
         secondsSinceMidnight = (hours * 3600 + minutes * 60 + seconds) % SECONDS_PER_DAY;
     }
 
+    /**
+     * Get the time of day.
+     * @return an array of size 3:
+     *    <blockquote>
+     *    [ <em>hours (0-23)</em>, <em>minutes (0-59)</em>, <em>seconds (0-59)</em> ]
+     *    </blockquote>
+     * Note that this returned array is pre-allocated, and whenever this method is called the values in
+     * the array will be overwritten.
+     */
+    final public int[] getTime() {
+        int t = secondsSinceMidnight;
+        final int seconds = t % 60;
+        t /= 60; // t = minutes since midnight
+        final int minutes = t % 60;
+        t /= 60; // t = hours since midnight
+        final int hours = t;
+        timeArray[0] = hours;
+        timeArray[1] = minutes;
+        timeArray[2] = seconds;
+        return timeArray;
+    }
+
     /** Set whether we used 24H format. */
-    public void setFormat24h(boolean format24h) {
+    final public void setFormat24h(boolean format24h) {
         this.format24h = format24h;
+    }
+
+    /** Set whether we should show the seconds in {@link #getTimeString}. */
+    final public void setShowSeconds(boolean showSeconds) {
+        this.showSeconds = showSeconds;
     }
 
     /**
@@ -86,16 +100,18 @@ public class TimeOfDayHandler implements TurnListener, StatusBarProvider
      * @return current time as a string
      */
     public String getTimeString() {
-        StringBuilder sb = new StringBuilder(10);
-        final int hours = getHourOfDay();
-        final int minutes = (secondsSinceMidnight % 3600) / 60;
+        final int[] ta = getTime();
+        final int hours = ta[0];
+        final int minutes = ta[1];
+        final int seconds = ta[2];
+        StringBuilder sb = new StringBuilder(16);
         if (format24h) {
-            if (hours < 10)
-                sb.append('0');
-            sb.append(hours).append(':');
-            if (minutes < 10)
-                sb.append('0');
-            sb.append(minutes);
+            append2(hours, sb).append(':');
+            append2(minutes, sb);
+            if (showSeconds) {
+                sb.append(':');
+                append2(seconds, sb);
+            }
         } else {
             if (hours == 0)
                 sb.append("12");
@@ -104,10 +120,12 @@ public class TimeOfDayHandler implements TurnListener, StatusBarProvider
             else
                 sb.append(hours);
             sb.append(':');
-            if (minutes < 10)
-                sb.append('0');
-            sb.append(minutes).append(' ');
-            sb.append(hours < 12 ? "AM" : "PM");
+            append2(minutes, sb);
+            if (showSeconds) {
+                sb.append(':');
+                append2(seconds, sb);
+            }
+            sb.append(' ').append(hours < 12 ? "AM" : "PM");
         }
         return sb.toString();
     }
@@ -124,7 +142,7 @@ public class TimeOfDayHandler implements TurnListener, StatusBarProvider
      * By default the value is {@code UIConstants.RIGHT_LABEL}.
      * @param statusLabelPos one of the <tt>LABEL</tt> positions in {@code UIConstants}
      */
-    public void setStatusLabelPos(int statusLabelPos) {
+    final public void setStatusLabelPos(int statusLabelPos) {
         this.statusLabelPos = statusLabelPos;
     }
 
@@ -137,14 +155,25 @@ public class TimeOfDayHandler implements TurnListener, StatusBarProvider
     }
 
     public Object getHandlerState() {
-        return Integer.valueOf(secondsSinceMidnight);
+        final Object[] oa = new Object[2];
+        oa[0] = Integer.valueOf(secondsSinceMidnight);
+        oa[1] = Integer.valueOf(secondsPerTurn);
+        return oa;
     }
 
     public void restoreHandlerState(Object state) {
-        secondsSinceMidnight = ((Integer) state).intValue();
+        final Object[] oa = (Object[]) state;
+        secondsSinceMidnight = ((Integer) oa[0]).intValue();
+        secondsPerTurn = ((Integer) oa[1]).intValue();
     }
 
     public void gameHandlerStarting(boolean newGame) {
         // empty
+    }
+
+    private StringBuilder append2(int val, StringBuilder sb) {
+        if (val < 10)
+            sb.append('0');
+        return sb.append(val);
     }
 }
